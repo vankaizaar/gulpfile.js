254e9140068d2f4f9d0f08056b6d77a1c7e69b7c
vladislav <Vladislavglumov@list.ru>
2018-11-19 02:28:04 +0300

    fixed bug

--- gulpfile.js
+++ gulpfile.js
@@ -35,6 +35,7 @@ var path = {
     src: {
         html: 'src/*.html',
         js: 'src/js/main.js',
+        js-lib: 'src/js/libs/*.js',
         style: 'src/style/main.scss',
         css: 'src/style/libs/*.css',
         img: 'src/img/**/*.*',
@@ -84,6 +85,12 @@ gulp.task('js:build', function () {
         .pipe(reload({stream: true})); 
 });
 
+gulp.task('js-lib:build', function () {
+    gulp.src(path.src.js-lib) 
+        .pipe(gulp.dest(path.build.js)) 
+        .pipe(reload({stream: true}));
+});
+
 gulp.task('style:build', function () {
     gulp.src(path.src.style) 
         .pipe(sourcemaps.init())
@@ -121,6 +128,7 @@ gulp.task('fonts:build', function() {
 gulp.task('build', [
     'html:build',
     'js:build',
+    'js-lib:build',
     'style:build',
     'css:build',
     'fonts:build',
